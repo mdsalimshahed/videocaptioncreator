026b5492c862a339007845b32a816d50dbe8ee81
Md Salim Shahed Shajid <mdsalimshahed@gmail.com>
2025-09-19 22:47:26 +0600

    Update app.js

--- app.js
+++ app.js
@@ -158,8 +158,6 @@ const app = {
                 this.logo.classList.remove('animating');
                 this.landingPage.classList.add('hidden');
                 this.mainContent.classList.remove('hidden');
-                (adsbygoogle = window.adsbygoogle || []).push({}); // <-- ADD THIS LINE
-
 
                 if (window.animations && typeof window.animations.runWorkspaceIntroAnimation === 'function') {
                     window.animations.runWorkspaceIntroAnimation();
@@ -170,7 +168,6 @@ const app = {
             this.logo.classList.remove('animating');
             this.landingPage.classList.add('hidden');
             this.mainContent.classList.remove('hidden');
-            (adsbygoogle = window.adsbygoogle || []).push({});
             setupPlayer();
         }
     },
